1cd86971b7833fb253bc2be253934adba4ec2cb5
유주 <ehirbl@gmail.com>
2024-06-17 04:38:33 +0900

    Update hook.cpp

--- module/src/main/cpp/hook.cpp
+++ module/src/main/cpp/hook.cpp
@@ -31,7 +31,7 @@
 #include "Rect.h"
 #include <fstream>
 #include <limits>
-#define GamePackageName "com.kakaogames.gdts" // define the game package name here please
+#define GamePackageName "com.percent.aos.luckydefense" // define the game package name here please
 
 int glHeight, glWidth;
 
